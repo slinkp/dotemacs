6f7b554c1542e2b412db5228a6ed5ad79a9c3379
Paul Winkler <paul.winkler@shopify.com>
2020-11-13 11:12:27 -0500

    .emacs.d snapshot prior to upgrade 26 to 27

---
+++ .emacs.d/elpa/git-commit-20200828.1753/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20200828.1753" "Edit Git commit messages" '((emacs "25.1") (dash "20200524") (transient "20200601") (with-editor "20200522")) :commit "b18be6c1464060249692acb41b2bc6e43e48fdea" :keywords '("git" "tools" "vc") :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
--- .emacs.d/elpa/grip-mode-20191114.1754/grip-mode-autoloads.el
+++
@@ -1,40 +0,0 @@
-;;; grip-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-(add-to-list 'load-path (directory-file-name
-                         (or (file-name-directory #$) (car load-path))))
-
-
-;;;### (autoloads nil "grip-mode" "../../../../../.emacs.d/elpa/grip-mode-20191114.1754/grip-mode.el"
-;;;;;;  "ca422959f3ee2b57b809311c34d3d4c9")
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/grip-mode-20191114.1754/grip-mode.el
-
-(autoload 'grip-mode "grip-mode" "\
-Live Markdown preview with grip.
-
-\(fn &optional ARG)" t nil)
-
-;;;### (autoloads "actual autoloads are elsewhere" "grip-mode" "../../../../../.emacs.d/elpa/grip-mode-20191114.1754/grip-mode.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/grip-mode-20191114.1754/grip-mode.el
-
-(if (fboundp 'register-definition-prefixes) (register-definition-prefixes "grip-mode" '("grip-")))
-
-;;;***
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/grip-mode-20191114.1754/grip-mode-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/grip-mode-20191114.1754/grip-mode.el")
-;;;;;;  (0 0 0 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; grip-mode-autoloads.el ends here
